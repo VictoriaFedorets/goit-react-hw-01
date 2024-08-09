eca55deda06eeb43f0da3e7125ff2030d219b1ba
Victoria <vika96karnauhova@gmail.com>
2024-08-10 00:59:03 +0300

    add 2 task

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,25 @@
+export default function TransactionHistory({ items }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(({ type, amount, currency, id }) => {
+          return (
+            <tr key={id}>
+              <td>{type}</td>
+              <td>{amount}</td>
+              <td>{currency}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
